121c5a55ac354557ebdd3395016ad7750cea0902
Sergey Utka <1sergeyutka1@gmail.com>
2022-11-16 14:22:37 +0300

    Add CoroutineExceptionHandler

--- app/src/main/java/com/example/kot/view_model/ViewModel.kt
+++ app/src/main/java/com/example/kot/view_model/ViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.kot.view_model
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.kot.model.SimpleDataModel
@@ -12,7 +13,12 @@ class SimpleViewModel(var num: Int = 0) : ViewModel() {
 
     val dataModelLiveData = MutableLiveData<SimpleDataModel>()
     val numLiveData = MutableLiveData<Int>()
+    val isSuccessLoadingLiveData  = MutableLiveData<Boolean>()
     var job: Job? = null
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.d("ERROR COROUTINE!", throwable.localizedMessage as String)
+    }
 
     fun getAllData() {
 
@@ -20,13 +26,16 @@ class SimpleViewModel(var num: Int = 0) : ViewModel() {
 
         job = CoroutineScope(Dispatchers.IO).launch {
             val response = dataApi.getSimpleData()
-            withContext(Dispatchers.Main) {
+            withContext(Dispatchers.Main + exceptionHandler) {
                 if (response.isSuccessful) {
                     dataModelLiveData.postValue(response.body())
+                    isSuccessLoadingLiveData.value = true
 //                    Log.d("CODE", response.code().toString())
 //                    Log.d("BODY().TITLE", response.body()?.title.toString())
                 } else {
                     Log.d("WWW", "Response is not success")
+                    isSuccessLoadingLiveData.value = false
+
                 }
             }
         }
